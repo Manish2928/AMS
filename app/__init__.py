6614e2001d23e9742727d5cff8473f81d644f5ed
manish2928 <manishkashyap9611@gmail.com>
2025-08-14 11:51:37 +0530

    Updated project files

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+    
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
+    
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
+    
+    @app.route("/base")
+    def base():
+        return render_template("base.html")
+    
+
+    return app
